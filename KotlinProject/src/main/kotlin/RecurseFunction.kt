15e95dc5da76e0e57c0ac7fd3a9c56478e58ae63
wbham <wbham97@naver.com>
2023-03-01 02:06:51 +0900

    일반,꼬리 재귀함수

---
+++ KotlinProject/src/main/kotlin/RecurseFunction.kt
@@ -0,0 +1,29 @@
+fun main() {
+    normalFactorial()
+    println()
+    tailRecFactorial()
+}
+
+// 일반적인 factorial의 재귀함수 만들기
+fun normalFactorial() {
+    val number = 4
+    val result: Long
+
+    result = factorial1(number)
+    println("Factorial: $number -> $result")
+}
+
+fun factorial1(n: Int): Long {
+    return if (n == 1) n.toLong() else n * factorial1(n - 1)
+}
+
+// 꼬리 재귀로 스택 오버플로 방지하기
+fun tailRecFactorial() {
+    val number = 5
+    println("Factorial: $number -> ${factorial2(number)}")
+}
+
+// 매개변수의 변화: 받아들일 값인 n, 실행할 run, 기본값은 1.
+tailrec fun factorial2(n: Int, run: Int = 1): Long {
+    return if (n == 1) run.toLong() else factorial2(n - 1, run * n)
+}
