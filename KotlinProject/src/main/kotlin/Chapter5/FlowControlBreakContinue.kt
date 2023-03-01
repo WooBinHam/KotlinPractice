15f4b35b6b4b8a782732394589f3bc639cd634c4
wbham <wbham97@naver.com>
2023-03-01 15:18:29 +0900

    FlowControl Break Continue with Label or not

---
+++ KotlinProject/src/main/kotlin/Chapter5/FlowControlBreakContinue.kt
@@ -0,0 +1,45 @@
+package Chapter5
+
+fun main() {
+    for (i in 1..5) {
+        if (i == 3) break
+        print("$i ")
+    }
+    println()
+    println("outside")
+
+    for (i in 1..5) {
+        if (i == 3) continue
+        print("$i ")
+    }
+    println()
+    println("outside")
+
+    labelNoBreak()
+    labelYesBreak()
+}
+
+// 라벨이 없는 break와 라벨이 있는 break
+fun labelNoBreak() {
+    println("labelNoBreak")
+    for (i in 1..5) {
+        second@ for (j in 1..5) {
+            if (j == 3) break
+            println("i: $i, j: $j")
+        }
+        println("after for j")
+    }
+    println("after for i")
+}
+
+fun labelYesBreak() {
+    println("labelYesBreak")
+    first@ for (i in 1..5) {
+        second@ for (j in 1..5) {
+            if (j == 3) break@first
+            println("i: $i, j: $j")
+        }
+        println("after for j")
+    }
+    println("after for i")
+}
