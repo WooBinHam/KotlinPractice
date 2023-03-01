bfffd749ab1b41e3eab4e695f272f66543d57178
wbham <wbham97@naver.com>
2023-03-01 15:47:18 +0900

    Try-Catch-Finally Exception
    - ArithmeticException Try-Catch
    - StackTrace Try-Catch
    - Using Throw to make Exception

---
+++ KotlinProject/src/main/kotlin/Chapter5/ExceptionTryCatch.kt
@@ -0,0 +1,68 @@
+package Chapter5
+
+fun main() {
+    basicTryCatch()
+    arithmeticExceptionTryCatch()
+    stackTraceTryCatch()
+
+    // throw를 사용해서 예외 발생시키기
+    var amount = 600
+    try {
+        amount-=100
+        checkAmount(amount)
+    }catch (e:Exception){
+        println(e.message)
+    }
+    println("amount: $amount")
+}
+
+fun checkAmount(amount:Int){
+    if(amount<1000){
+        throw Exception("잔고가 $amount 으로 1000 이하입니다.")
+    }
+}
+
+fun basicTryCatch(){
+    val a = 6
+    val b = 0
+    var c: Int
+    try {
+        c = a / b
+        println("After Calculation")
+    } catch (e: Exception) {
+        println("Exception")
+    } finally {
+        println("Finally")
+    }
+}
+
+// 산술 연산에 대한 예외를 따로 특정해서 잡을 때
+fun arithmeticExceptionTryCatch(){
+    val a = 6
+    val b = 0
+    var c: Int
+    try {
+        c = a / b
+        println("After Calculation using ArithmeticException")
+    } catch (e: ArithmeticException) {
+        println("Exception using ArithmeticException")
+    } finally {
+        println("Finally using ArithmeticException")
+    }
+}
+
+fun stackTraceTryCatch(){
+    val a = 6
+    val b = 0
+    var c: Int
+    // 스택의 추적
+    try {
+        c = a / b
+        println("After Calculation using printStackTrace")
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("Exception using printStackTrace")
+    } finally {
+        println("Finally using printStackTrace")
+    }
+}
