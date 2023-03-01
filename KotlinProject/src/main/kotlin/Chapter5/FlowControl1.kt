a3ba6040195f5bd6b88e13cfbd4d441e5f7ae9c5
wbham <wbham97@naver.com>
2023-03-01 14:59:44 +0900

    람다식, 익명함수 return 제어

--- KotlinProject/src/main/kotlin/Chapter5/FlowControl1.kt
+++ KotlinProject/src/main/kotlin/Chapter5/FlowControl1.kt
@@ -4,6 +4,7 @@ fun main() {
     retFunc1()
     retFunc2()
     retFunc3()
+    retFunc4()
 }
 
 // 람다식에서 return 사용하기
@@ -17,7 +18,7 @@ fun retFunc1() {
         if (result > 10) return             // 3번 10보다 크면 이 함수를 빠져 나감. 비지역 반환으로 retFunc 함수 자체가 끝난다.
         println("result: $result")          // 4번 10보다 크면 이 문장에 도달하지 못함
     }
-    println("end of retFunc")               // 5번
+    println("end of retFunc1")               // 5번
 }
 // -------------------------------------------------------------------------------------------------
 // 람다식에서 라벨 사용
@@ -31,19 +32,32 @@ fun retFunc2() {
         if (result > 10) return@lit           // 2번 라벨을 사용한 블록의 끝부분으로 반환(3번)
         println("result: $result")
     }                                         // 3번
-    println("end of retFunc")                 // 4번 이 부분이 실행됨
+    println("end of retFunc2")                 // 4번 이 부분이 실행됨
 }
 
 // 암묵적 라벨
 fun retFunc3() {
-    println("start of retFunc2")
+    println("start of retFunc3")
     inlineLambda2(13, 3) { a, b ->        // 1번 람다식 블록의 시작 부분에 라벨을 지정함
         val result = a + b
         if (result > 10) return@inlineLambda2   // 2번 라벨을 사용한 블록의 끝부분으로 반환(3번)
         println("result: $result")
     }                                         // 3번
-    println("end of retFunc")                 // 4번 이 부분이 실행됨
+    println("end of retFunc3")                 // 4번 이 부분이 실행됨
 }
+
+// 익명함수의 사용
+fun retFunc4() {
+    println("start of retFunc4")
+    inlineLambda2(13, 3, fun(a, b) {
+        val result = a + b
+        if (result > 10) return
+        println("result: $result")
+    })  // inlineLambda() 함수의 끝
+    println("end of retFunc4")
+}
+// 람다식 대신에 일반적인 익명함수를 넣어줌. a,b를 받고 구현부는 람다식과 동일
+// 이 함수 내에서 return 하게 되면 비지역 반환이 일어나지 않는다. 라벨을 사용하지 않아도 됨.
 // -------------------------------------------------------------------------------------------------
 fun add(a: Int, b: Int): Int {
     return a + b
