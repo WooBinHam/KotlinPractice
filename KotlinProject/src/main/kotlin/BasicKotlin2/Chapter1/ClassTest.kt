e8643091541c14f15d76518145f525a925697fce
wbham <wbham97@naver.com>
2023-03-06 16:01:25 +0900

    class

---
+++ KotlinProject/src/main/kotlin/BasicKotlin2/Chapter1/ClassTest.kt
@@ -0,0 +1,19 @@
+package BasicKotlin2.Chapter1
+
+// 클래스 정의
+class Car {
+    val wheel: Int = 4
+
+    fun start() {
+        println("Engine Start!")
+    }
+}
+
+class Bird
+
+// 객체 정의
+fun main(){
+    val sonata: Car = Car()
+    println(sonata.wheel)
+    sonata.start()
+}
