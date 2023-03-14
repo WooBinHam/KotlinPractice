20536f3ba14e36072ed109204b251dde1d48956c
wbham <wbham97@naver.com>
2023-03-14 17:16:00 +0900

    주석 추가

--- KotlinProject/src/main/kotlin/BasicKotlin2/Chapter1/Super.kt
+++ KotlinProject/src/main/kotlin/BasicKotlin2/Chapter1/Super.kt
@@ -20,26 +20,26 @@ class Developer : Person1 {
     }
 }
 
-class Person2(firstName: String, out: Unit = println("[Primary Constructor] Parameter")) {
-    val frame = println("[Property] Person fName: $firstName")
+class Person2(firstName: String, out: Unit = println("[Primary Constructor] Parameter")) {      // 2) 주 생성자
+    val frame = println("[Property] Person fName: $firstName")      // 3) 프로퍼티 할당
 
     init {
-        println("[init] Person init block")
+        println("[init] Person init block")     // 4) 초기화 블록
     }
-
+    // 1) 부 생성자
     constructor(
         firstName: String,
         age: Int,
         out: Unit = println("[Secondary Constructor] Parameter")
     ) : this(firstName) {
-        println("[Secondary Constructor] Body: $firstName, $age")
+        println("[Secondary Constructor] Body: $firstName, $age")       // 5) 부 생성자 본문
     }
 }
 
 fun main() {
     val sean = Developer("Sean")
     println()
-    val p1 = Person2("Kildong", 30)
+    val p1 = Person2("Kildong", 30)     // 1->2 호출, 3->4->5 실행
     println()
-    val p2 = Person2("Dooly")
+    val p2 = Person2("Dooly")       // 2 호출, 3->4 실행
 }
